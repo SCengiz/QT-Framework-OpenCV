f34173c7c93f91492565eb570e7857be24fba0f0
SCengiz <soraycengiz23@gmail.com>
2022-12-24 09:28:56 +0300

    Revert "observer pattern implementation."
    This reverts commit 05a49161bb15d623b61ffad00e88f27c56356be3.

---
+++ 001usingOpencvInQT/main.cpp
@@ -0,0 +1,14 @@
+#include <QCoreApplication>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    cv::Mat img = cv::imread("/home/htr/Downloads/lena.png", cv::IMREAD_GRAYSCALE);
+    cv::imshow("Window", img);
+    cv::waitKey(0);
+
+    return a.exec();
+}
